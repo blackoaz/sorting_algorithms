369f299459ccfa50ee4f6ac302b1922ba760462b
blackoaz <pauloakello56@gmail.com>
2023-04-26 06:34:15 -0700

    updates to the insertion sort algorithm of a doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,41 @@
 #include "sort.h"
+
 /**
- * swap_nodes - function for swapping nodes
- * @head: head of the list
- * @val1: the first node
- * @val2: the second node
+ * swap_node - function for swapping a doubly linked list
+ * @list: list to be sorted
+ * @first_n: left list to be sorted
+ * @next_n: next list to be sorted
  * Return: returns a void
  */
-void swap_nodes(listint_t **list, listint_t *val1, listint_t *val2)
+void swap_node(listint_t **list, listint_t *first_n, listint_t *next_n)
 {
-	val1->next = val2->next;
-	if (val2->next != NULL)
-		val2->next = val1;
-	val2->prev = val1->prev;
-	val2->next = val1;
-
-	if (val1->prev != NULL)
-		val1->prev->next = val2;
+	first_n->next = next_n->next;
+	if (next_n->next != NULL)
+		next_n->next->prev = first_n;
+	next_n->prev = first_n->prev;
+	next_n->next = first_n;
+	if (first_n->prev != NULL)
+		first_n->prev->next = next_n;
 	else
-		*list = val2;
-	val1->prev = val2;
-	val1 = val2->prev;
+		*list = next_n;
+	first_n->prev = next_n;
+	first_n = next_n->prev;
 }
 
 /**
- * insertion_sort_list - function for performing insertion sort
+ * insertion_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Insertion sort algorithm
+ *
  * @list: list to be sorted
  * Return: returns a void
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *node, *next;
 
+
 	node = malloc(sizeof(listint_t));
 	next = malloc(sizeof(listint_t));
-
 	if (list == NULL || node == NULL || next == NULL)
 		return;
 	node = *list;
@@ -44,7 +45,7 @@ void insertion_sort_list(listint_t **list)
 		next = node->prev;
 		if (next->n > node->n)
 		{
-			swap_nodes(list, next, node);
+			swap_node(list, next, node);
 			print_list(*list);
 			node = (*list)->next;
 		}
